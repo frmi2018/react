5d7cc32f4307f1277c956b53ef93be88193034c3
frmi <frmi2018@gmail.com>
2021-09-28 17:39:32 +0200

    added project

---
+++ front/react-vitrine/src/pages/Contact.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Mouse from "../components/Mouse";
+
+const Contact = () => {
+  return <Mouse />;
+};
+
+export default Contact;
---
+++ front/react-vitrine/src/pages/Portfolio.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Portfolio = () => {
+  return <div>Porfolio</div>;
+};
+
+export default Portfolio;
